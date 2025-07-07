bccb8df80360db2fc6f7dbb5279834c92a0ca741
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-07-07 12:05:12 +0200

    feat(DeclarationApp.java): complete TP 3 implementation

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,29 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 1;
+
+        short b = 12;
+
+        int c = 15000;
+
+        long d = 1000000;
+
+        float e = 52.68F;
+
+        double f = 122222222;
+
+        char g = 'G';
+
+        boolean h = true;
+
+        String i = "J'ai fini le TP !";
+
+        System.out.println(i);
+
+        String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+
+        System.out.println(randomString);
+    }
+}
